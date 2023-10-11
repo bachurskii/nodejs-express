9600652206bce98232e306016c19c9c3778450fc
bachurskii <georgijbacurskij@gmail.com>
2023-10-11 13:11:32 +0300

    new repositorii

--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,74 @@
-// const fs = require('fs/promises')
+import { readFile, writeFile } from "fs/promises";
+import path from "path";
 
-const listContacts = async () => {}
+const contactsPath = path.resolve("models", "contacts.json");
 
-const getContactById = async (contactId) => {}
+const updateContacts = (contacts) =>
+  writeFile(contactsPath, JSON.stringify(contacts, null, 2));
 
-const removeContact = async (contactId) => {}
+export const listContacts = async () => {
+  const data = await readFile(contactsPath);
+  return JSON.parse(data);
+};
 
-const addContact = async (body) => {}
+export const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  const result = contacts.find((item) => item.id === contactId);
+  return result || null;
+};
 
-const updateContact = async (contactId, body) => {}
+export const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  contacts.splice(index, 1);
+  await updateContacts(contacts);
+  return "Contact deleted";
+};
 
-module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
-}
+export const addContact = async (body) => {
+  const { name, email, phone } = body;
+  if (!name || !email || !phone) {
+    return null;
+  }
+  const contacts = await listContacts();
+  const newContact = {
+    id: Date.now().toString(),
+    name,
+    email,
+    phone,
+  };
+  contacts.push(newContact);
+  await updateContacts(contacts);
+
+  return newContact;
+};
+
+export const updateContact = async (contactId, body) => {
+  if (!body) {
+    return null;
+  }
+
+  const { name, email, phone } = body;
+
+  if (!name && !email && !phone) {
+    return null;
+  }
+
+  const contacts = await listContacts();
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+
+  if (index === -1) {
+    return null;
+  }
+
+  if (name) contacts[index].name = name;
+  if (email) contacts[index].email = email;
+  if (phone) contacts[index].phone = phone;
+
+  await updateContacts(contacts);
+
+  return contacts[index];
+};
