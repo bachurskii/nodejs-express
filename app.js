d563afeedd57feaa3fe5488ac8dadfea8d75d0b3
bachurskii <georgijbacurskij@gmail.com>
2023-10-30 20:50:11 +0200

    add new merge

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 
 import contactsRouter from "./routes/api/contacts.js";
-
+import authRouter from "./routes/api/auth-router.js";
 dotenv.config();
 const app = express();
 
@@ -15,7 +15,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/contacts", contactsRouter);
-
+app.use("/api/auth", authRouter);
+app.use("/api/users", authRouter);
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
 });
---
+++ models/users.js
@@ -0,0 +1,47 @@
+import { Schema, model } from "mongoose";
+
+import Joi from "joi";
+
+const emailRegexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+const userShema = new Schema(
+  {
+    password: {
+      type: String,
+      minlength: 6,
+      required: [true, "Password is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      match: emailRegexp,
+      unique: true,
+    },
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter",
+    },
+    token: {
+      type: String,
+      default: null,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+    },
+  },
+
+  { versionKey: false, timestamps: true }
+);
+
+export const userSignUpShema = Joi.object({
+  email: Joi.string().pattern(emailRegexp),
+  password: Joi.string().min(6).required(),
+});
+export const userSigningShema = Joi.object({
+  email: Joi.string().pattern(emailRegexp),
+  password: Joi.string().min(6).required(),
+});
+const User = model("user", userShema);
+
+export default User;
